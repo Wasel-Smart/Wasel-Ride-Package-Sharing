0f7f1a2470f411abd03cad7618afbbe50e07d618
Wasel <YOUR_ACTUAL_NO_REPLY_EMAIL>
2026-08-09 06:01:45 +0300

    fix: clean App.tsx merge artifacts and remove temp debug files

--- tmp-fix-waselmap-imports.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = 'src/components/WaselMap.tsx';
-let content = fs.readFileSync(path, 'utf8');
-content = content.replace(
-  "import { JORDAN_RADARS, FALLBACK_MOSQUES, MAP_CENTER, DEFAULT_ZOOM, TILE_CONFIGS } from './MapConfig';",
-  "import { JORDAN_RADARS, FALLBACK_MOSQUES, TILE_CONFIGS } from './MapConfig';"
-);
-fs.writeFileSync(path, content);
-console.log('Removed unused imports');
