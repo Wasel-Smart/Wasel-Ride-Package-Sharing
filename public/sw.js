aa9b94041d4b1d1e8c5007194491f343ad2f80ad
Wasel <YOUR_ACTUAL_NO_REPLY_EMAIL>
2026-08-20 16:19:27 +0300

    yes

--- public/sw.js
+++ public/sw.js
@@ -34,8 +34,6 @@ const NETWORK_TIMEOUT_MS = 8000;
 // hiccuped, which is what produced app_mount_timeout on mobile.
 const PRECACHE_STATIC = [
   '/offline.html',
-  '/initial-locale.js',
-  '/boot-status.js',
   '/manifest.webmanifest',
   '/favicon.ico',
   '/favicon-16x16.png',
